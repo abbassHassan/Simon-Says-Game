feb6897e4e338db83ce3124d1c01e958c3ea074f
abbas hassan <abbass.hassan.a7@gmail.com>
2023-11-12 15:12:34 +0200

    start of the game + generate the random color

--- game.js
+++ game.js
@@ -15,3 +15,52 @@ function button_animation(audio) {
   }, 50);
 }
 
+// adding the click listener for the buttons
+red_btn.addEventListener("click", function () {
+  button_animation.call(this, "redAudio");
+  buttonClicked.call(this);
+});
+green_btn.addEventListener("click", function () {
+  button_animation.call(this, "greenAudio");
+  buttonClicked.call(this);
+});
+blue_btn.addEventListener("click", function () {
+  button_animation.call(this, "blueAudio");
+  buttonClicked.call(this);
+});
+yellow_btn.addEventListener("click", function () {
+  button_animation.call(this, "yellowAudio");
+  buttonClicked.call(this);
+});
+
+let gameOver = true;
+let level;
+let colorsOrder;
+let current;
+let userInput;
+let randColor;
+let colors = ["green", "red", "blue", "yellow"];
+
+document.addEventListener("keydown", function () {
+  if (gameOver) {
+    start();
+  }
+});
+
+function generateRandom() {
+  randColor = colors[Math.floor(Math.random() * 4)];
+  colorsOrder.push(randColor);
+  button_animation.call(
+    document.getElementById(randColor),
+    randColor + "Audio"
+  );
+}
+
+function start() {
+  colorsOrder = [];
+  gameOver = false;
+  level = 1;
+  title.textContent = "Level " + level;
+  current = 0;
+  generateRandom();
+}
